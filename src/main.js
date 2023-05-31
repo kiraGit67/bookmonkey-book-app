9098e5781a5346d469992e86df1c57eee8af275c
kiraGit67 <kirsten@sais.de>
2023-05-31 20:42:55 +0200

    imports router system, HomeView and AboutView into main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+    component: HomeView,
+  },
+  {
+    name: "About",
+    path: "/about",
+    component: AboutView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
