a229237d2465b39d6e444c599a733dc1a9c4fdfa
kiraGit67 <kirsten@sais.de>
2023-05-31 21:31:34 +0200

    adds src/views/BookView.vue

---
+++ src/views/BookView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Book View</h1>
+  <p>{{ this.$route.params }}</p>
+  <pre>
+      {{ book }}
+  </pre>
+</template>
+
+<script>
+export default {
+  name: "BookView",
+  data() {
+    return {
+      book: {},
+    };
+  },
+  created() {
+    fetch("http://localhost:4730/books/" + this.$route.params.isbn)
+      .then((res) => res.json())
+      .then((book) => (this.book = book));
+  },
+};
+</script>
