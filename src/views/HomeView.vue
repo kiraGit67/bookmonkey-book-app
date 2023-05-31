522505849d4a6a290d71c6aae4ea0f3bde37316d
kiraGit67 <kirsten@sais.de>
2023-05-31 21:32:42 +0200

    adds book data and created hook into HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <template>
   <h1>Home View</h1>
+  <ul>
+    <li v-for="book in books" :key="book.id">
+      {{ book.title }}
+      <router-link :to="{ name: 'details', params: { isbn: book.isbn } }"
+        >View</router-link
+      >
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  data() {
+    return {
+      books: [],
+    };
+  },
+  created() {
+    fetch("http://localhost:4730/books")
+      .then((res) => res.json())
+      .then((books) => (this.books = books));
+  },
 };
 </script>
