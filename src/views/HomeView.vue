dfadcbb8c6d0084912ab75f4485a188e10514840
kiraGit67 <kirsten@sais.de>
2023-06-01 19:55:02 +0200

    modifies template in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Home View</h1>
+  <h1>{{ h1title }}</h1>
   <ul>
     <li v-for="book in books" :key="book.id">
-      {{ book.title }}
-      <router-link :to="{ name: 'details', params: { isbn: book.isbn } }"
-        >View</router-link
-      >
+      <div class="book-img">
+        <img :src="book.cover" :alt="book.title" :title="book.title" />
+      </div>
+      <div class="book-details">
+        <h3>{{ book.title }}</h3>
+        <h4>{{ book.subtitle }}</h4>
+        <h5>{{ book.author }}</h5>
+        <router-link :to="{ name: 'details', params: { isbn: book.isbn } }"
+          >Details</router-link
+        >
+      </div>
     </li>
   </ul>
 </template>
@@ -15,6 +22,7 @@ export default {
   name: "HomeView",
   data() {
     return {
+      h1title: "BookMonkey Book Store",
       books: [],
     };
   },
@@ -25,3 +33,34 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+}
+
+li {
+  display: grid;
+  grid-template-columns: 15% 85%;
+  grid-column-gap: 0.75rem;
+  align-items: center;
+  padding: 0.75rem 0;
+  border-bottom: 1px solid #42b983;
+}
+
+img {
+  max-width: 100%;
+}
+
+h3,
+h4,
+h5 {
+  margin: 0.25rem 0;
+}
+
+router-link {
+  text-decoration: none;
+  color: #42b983;
+}
+</style>
