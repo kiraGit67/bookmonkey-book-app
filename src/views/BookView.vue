5501496d63d01604588753b89f9ac99699e18b87
kiraGit67 <kirsten@sais.de>
2023-06-02 23:23:10 +0200

    adds scoped styling

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>Book View</h1>
+  <h1>{{ book.title }}</h1>
+  <div class="book-container">
+    <div class="book-container--book-image">
+      <img :src="book.cover" :alt="book.title" :title="book.title" />
+    </div>
+    <div class="book-container--book-details">
+      <h2>{{ book.subtitle }}</h2>
+      <p>{{ book.abstract }}</p>
+      <ul class="book-details--list">
+        <li>
+          <span><strong>Author</strong></span
+          ><span>{{ book.author }}</span>
+        </li>
+        <li>
+          <span><strong>Publisher</strong></span
+          ><span>{{ book.publisher }}</span>
+        </li>
+        <li>
+          <span><strong>Pages</strong></span
+          ><span>{{ book.numPages }}</span>
+        </li>
+        <li>
+          <span><strong>Price</strong></span
+          ><span>{{ book.price }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- 
   <p>{{ this.$route.params }}</p>
   <pre>
       {{ book }}
   </pre>
+  -->
 </template>
 
 <script>
@@ -21,3 +50,35 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul.book-details--list {
+  list-style-type: none;
+  padding-left: 0;
+}
+
+.book-details--list > li {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+}
+
+.book-details--list > li strong,
+h1,
+h2 {
+  color: #42b983;
+}
+
+h1,
+h2 {
+  font-weight: 500;
+}
+
+@media screen and (min-width: 768px) {
+  .book-container {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 0.5rem;
+    align-items: center;
+  }
+}
+</style>
