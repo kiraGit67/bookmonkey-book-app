5a17ed0868516b57a0a4840b3890afea2eff638d
kiraGit67 <kirsten@sais.de>
2023-06-03 19:52:04 +0200

    adds router-link to AddBookView into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/books/add">Add New Book</router-link>
   </nav>
   <router-view></router-view>
 </template>
